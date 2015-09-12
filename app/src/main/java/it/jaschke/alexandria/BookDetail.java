36b97fcd683dc3ace87ef2429ec650eb533dd468
Jose Lopez <joselopezrosario@yahoo.com>
2015-09-12 14:52:05 -0500

    I fixed a bug that made the app crashed when changing the orientation on a BookDetail. It was caused by the ShareActionProvider not being static. I also disabled the back button view as per Android Material Design we should use the default back functionality provided by Android, via the device's back button or the Action Bar.

--- app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -8,6 +8,7 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -30,8 +31,9 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
     private View rootView;
     private String ean;
     private String bookTitle;
-    private ShareActionProvider shareActionProvider;
-
+    // I changed this variable to static to fix crash on orientation change
+    private static ShareActionProvider shareActionProvider;
+    private static final String LOG_TAG = "BookDetail";
     public BookDetail(){
     }
 
@@ -68,8 +70,8 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        Log.e(LOG_TAG, "onCreateOptionsMenu -> " + true);
         inflater.inflate(R.menu.book_detail, menu);
-
         MenuItem menuItem = menu.findItem(R.id.action_share);
         shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
     }
@@ -91,15 +93,22 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         if (!data.moveToFirst()) {
             return;
         }
-
+        Log.e(LOG_TAG, "onLoadFinished -> " + true);
         bookTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
         ((TextView) rootView.findViewById(R.id.fullBookTitle)).setText(bookTitle);
 
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
         shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text)+bookTitle);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
+        Log.e(LOG_TAG, "onLoadFinished sharedIntent-> " + shareIntent);
+        // The app crashed here when turning from portrait to landscape
+        // I changed the shareActionProvider to static to fix this
+        Log.e(LOG_TAG, "shareActionProvider-> " + shareActionProvider);
         shareActionProvider.setShareIntent(shareIntent);
+
+
+
 
         String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
         ((TextView) rootView.findViewById(R.id.fullBookSubTitle)).setText(bookSubTitle);
@@ -112,6 +121,8 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
         ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",","\n"));
         String imgUrl = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
+        // This doesn't work when there is not network connection
+        // Look into storing the images
         if(Patterns.WEB_URL.matcher(imgUrl).matches()){
             new DownloadImage((ImageView) rootView.findViewById(R.id.fullBookCover)).execute(imgUrl);
             rootView.findViewById(R.id.fullBookCover).setVisibility(View.VISIBLE);
@@ -120,9 +131,13 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         String categories = data.getString(data.getColumnIndex(AlexandriaContract.CategoryEntry.CATEGORY));
         ((TextView) rootView.findViewById(R.id.categories)).setText(categories);
 
+        /**
+         * I disabled the back button view because as per Android Material Design we shouldn't
+         * add back buttons to our app. We should use the default back button provided by the
+         * device or ActionBar
         if(rootView.findViewById(R.id.right_container)!=null){
             rootView.findViewById(R.id.backButton).setVisibility(View.INVISIBLE);
-        }
+        }**/
 
     }
 
